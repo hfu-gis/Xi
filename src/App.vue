54044f6105cb06f82c6c97a113d94144ac83e931
alicheaitou <Mv2@ha9gjdzs>
2019-12-04 10:50:08 +0100

    Ich habe den Worldbutton hinzugefügt.

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,16 @@
             <v-list-item-title>Contact</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-earth</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Map</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
       </v-list>
     </v-navigation-drawer>
 
