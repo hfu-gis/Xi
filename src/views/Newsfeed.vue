4bad8e3a4017255d578163189b6e3513e52275aa
kimbl <kim.julia.bloch@hs-furtwangen>
2019-12-19 17:11:31 +0100

    newsfeed

--- src/views/Newsfeed.vue
+++ src/views/Newsfeed.vue
@@ -1,5 +1,13 @@
 <template>
 
+    <v-app>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+
     <div id="app">
         <v-app id="inspire">
             <v-card
@@ -7,20 +15,21 @@
                     class="mx-auto"
             >
                 <v-list-item>
-                    <v-list-item-avatar color="grey"></v-list-item-avatar>
+                    <v-list-item-avatar color="red"></v-list-item-avatar>
                     <v-list-item-content>
-                        <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
-                        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+                        <v-list-item-title class="headline">Amazonas burning down</v-list-item-title>
+                        <v-list-item-subtitle>by Phil Taylor</v-list-item-subtitle>
                     </v-list-item-content>
                 </v-list-item>
 
                 <v-img
-                        src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
-                        height="194"
-                ></v-img>
+                 src="../../img/Amazonas.jpg" height="194" width="400"
+                 >
+
+                </v-img>
 
                 <v-card-text>
-                    Visit ten places on our planet that are undergoing the biggest changes today.
+                    The Amazonas is dying. What can we do to protect it?
                 </v-card-text>
 
                 <v-card-actions>
@@ -48,6 +57,59 @@
         </v-app>
     </div>
 
+
+
+
+        <div id="app">
+            <v-app id="inspire">
+                <v-card
+                        max-width="344"
+                        class="mx-auto"
+                >
+                    <v-list-item>
+                        <v-list-item-avatar color="red"></v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title class="headline">Amazonas burning down</v-list-item-title>
+                            <v-list-item-subtitle>by Phil Taylor</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-img
+                            src="../../img/Amazonas.jpg" height="194" width="400"
+                    >
+
+                    </v-img>
+
+                    <v-card-text>
+                        The Amazonas is dying. What can we do to protect it?
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-btn
+                                text
+                                color="deep-purple accent-4"
+                        >
+                            Read
+                        </v-btn>
+                        <v-btn
+                                text
+                                color="deep-purple accent-4"
+                        >
+                            Bookmark
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn icon>
+                            <v-icon>mdi-heart</v-icon>
+                        </v-btn>
+                        <v-btn icon>
+                            <v-icon>mdi-share-variant</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-app>
+        </div>
+
+    </v-app>
 </template>
 
 
