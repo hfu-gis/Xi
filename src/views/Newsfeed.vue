c0118c54775c5706c537830cdb00ab215bf8af29
kimbl <kim.julia.bloch@hs-furtwangen>
2020-01-14 19:41:01 +0100

    Weltkarte eingefügt

--- src/views/Newsfeed.vue
+++ src/views/Newsfeed.vue
@@ -71,15 +71,18 @@
     export default {
         name: "Newsfeed",
         computed: {
-            articles () {
+            articles() {
                 return this.$store.getters.loadedArticles
-            }
-        }
+            },
+        },
+
     }
 
 </script>
 
 <style scoped>
+
+
     body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
 
